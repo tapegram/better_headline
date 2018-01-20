7d306ecf9350c16894ba1f335beb088cb247fa4f
tapegram <tapegram@gmail.com>
2018-01-20 14:47:48 -0500

    use postgres and install psycopg2

--- better_headline/settings.py
+++ better_headline/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'better_headline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'better_headline_db',
+        'USER': 'admin',
+        'PASSWORD': 'tavish',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
