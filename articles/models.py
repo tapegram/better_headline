92fa0d5c8e2dbc98b7d3623cec859194f909cb0f
tapegram <tapegram@gmail.com>
2018-01-20 15:17:27 -0500

    add str to models

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,13 @@ class Article(models.Model):
     article_url = models.URLField(max_length=2000)
     title = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     text = models.TextField()
+
+    def __str__(self):
+        return self.text[:25]
