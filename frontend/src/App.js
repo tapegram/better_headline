222ec3c6eaad274a713c66b007fecbbaade7ca6c
tapegram <tapegram@gmail.com>
2018-01-23 22:54:08 -0500

    messing with semantic ui components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import ItemExampleHeaders from './articles/article_item';
+
 
 class App extends Component {
     render() {
         return (
                 <div>
                 <button type="button" onClick={this.onClick}>Send GET /articles </button>
+                <ItemExampleHeaders />
                 </div>
         );
     }
